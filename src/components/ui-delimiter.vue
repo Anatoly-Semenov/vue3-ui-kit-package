c8f5aee3e070722ac22b39a4cbf78663df1e3a58
Anatoly Semenov <anatolysrmenov@gmail.com>
2022-09-25 18:55:19 +0300

    Create u-delimiter component

---
+++ src/components/ui-delimiter.vue
@@ -0,0 +1,32 @@
+<template>
+	<hr
+		class="ui-delimiter"
+		:class="`_${type}`"
+		role="separator"
+		aria-orientation="horizontal"
+	/>
+</template>
+
+<script setup lang="ts">
+import { defineProps, PropType } from "vue"
+
+const props = defineProps({
+	type: {
+		type: String as PropType<"solid" | "dashed">,
+		required: true,
+		default: "solid"
+	}
+})
+</script>
+<style lang="stylus">
+@import "@/assets/styles/base/variables.styl"
+
+.ui-delimiter
+	border none
+	border-top-style solid
+	border-top-color $color-border
+	border-top-width 1px
+
+	&._dashed
+		border-top-style dashed
+</style>
